b24006166ea41152dc3f386db67e1532742c27f9
Replit user <>
2023-02-07 18:16:29 +0000

    feat: creating a model, mongoose

--- myApp.js
+++ myApp.js
@@ -3,7 +3,13 @@ let mongoose = require('mongoose')
 
 mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+let personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  favoriteFoods: []
+})
+
+let Person = mongoose.model("Person", personSchema)
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
